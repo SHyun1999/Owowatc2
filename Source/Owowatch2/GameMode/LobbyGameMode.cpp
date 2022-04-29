b2bb5024fa1259a1228fe98e02ce21e49c8189da
SHyun1999 <shus009@gmail.com>
2022-04-28 19:09:11 -0500

    add map and seamless travel

---
+++ Source/Owowatch2/GameMode/LobbyGameMode.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LobbyGameMode.h"
+#include "GameFramework/GameState.h"
+
+void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	int32 NrOfPlayers = GameState.Get()->PlayerArray.Num();
+
+	if (NrOfPlayers == 2)
+	{
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			bUseSeamlessTravel = true;
+			World->ServerTravel(FString("/Game/Maps/OwoMap?listen"));
+		}
+	}
+}
