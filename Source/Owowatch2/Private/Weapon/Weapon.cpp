e18efc30cebd9e177597e5a50efb5b88dce3d66a
SHyun1999 <shus009@gmail.com>
2022-04-30 15:12:42 -0500

    make pickup widget

--- Source/Owowatch2/Private/Weapon/Weapon.cpp
+++ Source/Owowatch2/Private/Weapon/Weapon.cpp
@@ -3,6 +3,8 @@
 
 #include "Weapon/Weapon.h"
 #include "Components/SphereComponent.h"
+#include "Components/WidgetComponent.h"
+#include "Owowatch2/Public/Character/OwoCharacter.h"
 // Sets default values
 AWeapon::AWeapon()
 {
@@ -14,16 +16,19 @@ AWeapon::AWeapon()
 
 	//set mesh, weapons will be droppeable and pickable later on.
 	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
-	SetRootComponent(WeaponMesh);
 
 	WeaponMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
 	WeaponMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
 	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	SetRootComponent(WeaponMesh);
 
 	AreaSphere = CreateDefaultSubobject<USphereComponent>(TEXT("AreaSphere"));
-	AreaSphere->SetupAttachment(RootComponent);
 	AreaSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
 	AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	AreaSphere->SetupAttachment(RootComponent);
+
+	PickUpWidget = CreateDefaultSubobject<UWidgetComponent>(TEXT("PickUpWidget"));
+	PickUpWidget->SetupAttachment(RootComponent);
 
 }
 
@@ -31,12 +36,29 @@ void AWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 
+	if (PickUpWidget)
+	{
+		PickUpWidget->SetVisibility(false);
+	}
+
 	if (HasAuthority())
 	{
 		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 		AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+
+		AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnSphereOverlap);
 	}
 	
+}
+
+void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AOwoCharacter* OwoChar = Cast<AOwoCharacter>(OtherActor);
+	if (OwoChar && PickUpWidget)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("owow"));
+		PickUpWidget->SetVisibility(true);
+	}
 }
 
 // Called every frame
--- Source/Owowatch2/Public/Weapon/Weapon.h
+++ Source/Owowatch2/Public/Weapon/Weapon.h
@@ -33,6 +33,14 @@ protected:
 	
 	virtual void BeginPlay() override;
 
+	UFUNCTION()
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, 
+		AActor* OtherActor, 
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep, 
+		const FHitResult& SweepResult);
+
 private:
 	
 	UPROPERTY(VisibleAnywhere, Category = "Weapon")
@@ -44,6 +52,8 @@ private:
 	UPROPERTY(VisibleAnywhere)
 	EWeaponState WeaponState;
 
+	UPROPERTY(VisibleAnywhere, Category = "Weapon")
+	class UWidgetComponent* PickUpWidget;
 public:	
 	
 
